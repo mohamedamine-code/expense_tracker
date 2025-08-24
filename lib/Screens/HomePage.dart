65dfa0132eead4fbb01d8a1a870de59a1aa95032
mohamedamine-code <mohamedaminebensir09@gmail.com>
2025-08-24 12:49:43 +0100

    commit

--- lib/Screens/HomePage.dart
+++ lib/Screens/HomePage.dart
@@ -6,19 +6,62 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            _header(),
-          ],
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _header(),
+                SizedBox(height: 20),
+                Expanded(child: _TabBar()),
+              ],
+            ),
+          ),
         ),
       ),
     );
   }
-  Widget _header(){
-    return Text("Welcome To Expense Tracker",textAlign: TextAlign.center,style: mystyle(color: balck, fontWeight: bold, fontSize: 30),);
+
+  Widget _header() {
+    return Column(
+      // crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Welcome To Expense Tracker",
+          textAlign: TextAlign.center,
+          style: mystyle(color: balck, fontWeight: bold, fontSize: 30),
+        ),
+        SizedBox(height: 20),
+        Text("Welocme to the Expense Tracker, When you can "),
+        Text("trak your dailu, weeklu and monthly,"),
+        Text("your expense and be econmic"),
+      ],
+    );
+  }
+
+  Widget _TabBar() {
+    return Column(
+      children: [
+        TabBar(
+          tabs: [
+            Tab(text: "Expense List"),
+            Tab(text: "Total List"),
+          ],
+        ),
+        Expanded(
+          child: TabBarView(
+            children: [
+              Center(child: Text("1")),
+              Text("2"),
+            ],
+          ),
+        ),
+        
+      ],
+    );
   }
 }
